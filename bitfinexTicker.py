b7fd896be01c8d5bb589c2fa6d35dc0586ba9c07
nichtoune <nichtoune9999@yahoo.fr>
2018-03-29 20:21:33 +0200

    requete des 50 derniers trades à H-24

--- bitfinexTicker.py
+++ bitfinexTicker.py
@@ -1,12 +1,20 @@
 # -*- coding: utf8 -*-
 
 import urllib2
-contents = urllib2.urlopen("https://api.bitfinex.com/v1/trades/BTCUSD?limit_trades=5").read()
-print contents
-
+import time
 
 '''création du timestamp h-24'''
 now = time.time()
 tsnow = time.localtime(now)
 timerstart = time.mktime(tsnow)-86400
 print timerstart
+
+
+''' requête des 50 trades à partir du timestart'''
+
+tradeUrl = "https://api.bitfinex.com/v1/trades/BTCUSD?limit_trades=50&timestamp="
+
+contents = urllib2.urlopen(tradeUrl+str(timerstart)).read()
+print contents
+
+
