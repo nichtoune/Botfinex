c5dcc9b22b460887669098cc8ef9358c2d264029
nichtoune <nichtoune9999@yahoo.fr>
2018-03-29 20:06:22 +0200

    Variable qui contient le timestamp a H-24

--- bitfinexTicker.py
+++ bitfinexTicker.py
@@ -1,3 +1,12 @@
-import urllib.request
-contents = urllib.urlopen("https://api.bitfinex.com/v1/pubticker/BTCUSD").read()
+# -*- coding: utf8 -*-
+
+import urllib2
+contents = urllib2.urlopen("https://api.bitfinex.com/v1/trades/BTCUSD?limit_trades=5").read()
 print contents
+
+
+'''création du timestamp h-24'''
+now = time.time()
+tsnow = time.localtime(now)
+timerstart = time.mktime(tsnow)-86400
+print timerstart
